c9b20938495a480c2ddad4a297adf300d90e346f
Raynon Messias <raymessias@gmail.com>
2020-11-01 02:55:43 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,42 @@
 'use strict'
 
-import React, { Component } from 'react'
+import React from 'react'
 
-// ES6 Class
-class App extends Component {
-  render() {
-    return (
-      <fragment>
-        <div>Aplicação</div>
-        <div>Aplicação 2</div>
-      </fragment>
-    )
-  }
-}
-// Pure Function
-// const App = React.createClass({
-//   render: function () {
-//     return <div>Aplicação 2</div>
-//   }
-// })
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input
+        type='search'
+        placeholder='Digite o nome do usuário no Github' />
+    </div>
+    <div className='user-info'>
+      <img src='https://avatars3.githubusercontent.com/u/59324178?v=4'></img>
+      <h1>
+        <a href='https://github.com/raymessias'>Raynon Messias</a>
+      </h1>
+      <ul className='repos-info'>
+        <li>Repositórios: 122</li>
+        <li>Seguidores: 10</li>
+        <li>Seguindo: 10</li>
+      </ul>
+      <div className='actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
+      </div>
+      <div className='repos'>
+        <h2>Repositórios</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+      <div className='starred'>
+        <h2>Favoritos</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
 
 export default App
